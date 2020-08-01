40e272b97b61ada623c1c2e266d2042dc76848b5
Brian OLeary <oleary.brian1@gmail.com>
2020-08-01 17:03:53 -0400

    searching by zip code

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var APIKey = "3fa2a0131565ebb35436afe8cff1d40b";
 // let longitude = -71.0598;
 //   var queryURL = "api.openweathermap.org/data/2.5/forecast?q=boston&appid=" + APIKey;
 //   var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely&appid=" + APIKey;
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=scituate&units=imperial&appid=" + APIKey; //working api link
+// var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=scituate&units=imperial&appid=" + APIKey; //working api link
 // var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=" + APIKey
 // var secondQueryURL = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey)
 // var secondQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=42.3584&lon=-71.0598&appid=" + APIKey;
@@ -27,28 +27,14 @@ function secondAjaxCall(obj) {
     })
         .then(function (responsetwo) {
             console.log(responsetwo)
-            // console.log('lon ' + longitude)
 
+            var weatherImg = ("http://openweathermap.org/img/wn/" + responsetwo.current.weather[0].icon + "@2x.png")
 
-            // const moment = require('moment');
-            let i;
-            for (i = 1; i < 6; i++) {
-                var dateString = moment.unix(responsetwo.daily[i].dt).format("MM/DD/YYYY");
-                console.log(dateString)
-
-                const dayDiv = $('<div>').addClass('card text-white bg-info mb-3 fiveDayItem card');
-                const dayCardBodyDiv = $('<div>').addClass('card-body');
-                const date = $('<p>').addClass('bold card-title').text(dateString);
-                const dayImg = $('<img>').attr('src', ("http://openweathermap.org/img/wn/" + responsetwo.daily[i].weather[0].icon + "@2x.png")).attr('alt', responsetwo.daily[i].weather[0].main).width('50%');
-                const dayTemp = $('<p>').addClass('dayDisplay').text("Temp: " + ((responsetwo.daily[i].temp.day - 273) * 1.8 + 32).toFixed(2) + "°F");
-                const dayHumidity = $('<p>').addClass('dayDisplay').text("Humidity:" + responsetwo.daily[i].humidity + "%");
-
-
-                dayCardBodyDiv.append(date, dayImg, dayTemp, dayHumidity)
-                dayDiv.append(dayCardBodyDiv)
-                dayContainer.append(dayDiv)
-
-            }
+            // Transfer content to HTML for current
+            $(".cityCurrentSource").attr("src", weatherImg) //add image of weather
+            $(".currentTempDisplay").html("Temperture: " + ((responsetwo.current.temp - 273) * 1.8 + 32).toFixed(2) + "°F")
+            $(".currentHumidityDisplay").html("Humidity: " + responsetwo.current.humidity + "%")
+            $(".currentwindspeedDisplay").html("Wind speed: " + responsetwo.current.wind_speed + "mph")
             let highlightColor = ''
 
             if (responsetwo.current.uvi < 3) {
@@ -73,66 +59,56 @@ function secondAjaxCall(obj) {
 
             uvIndexContainer.append(indexdisplay);
 
-            $(".currentUVIndexDisplay").html("UV Index: " + responsetwo.current.uvi) //this is down here, because it comes from the second api call
+            //////////////////////5 day forcast below////////////////////////////////
+
+            let i;
+            for (i = 1; i < 6; i++) {
+                var dateString = moment.unix(responsetwo.daily[i].dt).format("MM/DD/YYYY");
+                console.log(dateString)
+
+                const dayDiv = $('<div>').addClass('card text-white bg-info mb-3 fiveDayItem card');
+                const dayCardBodyDiv = $('<div>').addClass('card-body');
+                const date = $('<p>').addClass('bold card-title').text(dateString);
+                const dayImg = $('<img>').attr('src', ("http://openweathermap.org/img/wn/" + responsetwo.daily[i].weather[0].icon + "@2x.png")).attr('alt', responsetwo.daily[i].weather[0].main).width('50%');
+                const dayTemp = $('<p>').addClass('dayDisplay').text("Temp: " + ((responsetwo.daily[i].temp.day - 273) * 1.8 + 32).toFixed(2) + "°F");
+                const dayHumidity = $('<p>').addClass('dayDisplay').text("Humidity:" + responsetwo.daily[i].humidity + "%");
+
+                dayCardBodyDiv.append(date, dayImg, dayTemp, dayHumidity)
+                dayDiv.append(dayCardBodyDiv)
+                dayContainer.append(dayDiv)
+
+            }
+
+
         });
 }
-
-function firstAjaxCall() {
-
-    // Here we run our AJAX call to the OpenWeatherMap API
+let zipCode = "02066"
+// function firstAjaxCall() {
     $.ajax({
-        url: queryURL,
+        url: (`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${APIKey}`),
         method: "GET"
     })
         // We store all of the retrieved data inside of an object called "response"
         .then(function (response) {
+            $(".cityCurrent").html(response.name + moment().format(' MMMM Do YYYY')); //add city and date
 
-            // Log the queryURL
-
-
-            // Log the resulting object
-            console.log(response.city.coord.lat)
-            console.log(response);
-
-            const latitude = (response.city.coord.lat);
-            const longitude = (response.city.coord.lon);
-
-            // console.log('lat '+ latitude)
-            var weatherImg = ("http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png")
-            // console.log(weatherImg)
+            const latitude = (response.coord.lat);
+            const longitude = (response.coord.lon);
             const location = {
                 latitude: latitude,
                 longitude: longitude
             }
             secondAjaxCall(location);
-            // Transfer content to HTML
-            $(".cityCurrent").html(response.city.name + moment().format(' MMMM Do YYYY')); //add city and date
-            $(".cityCurrentSource").attr("src", weatherImg) //add image of weather
-            $(".currentTempDisplay").html("Temperture: " + response.list[0].main.temp + "°F")
-            $(".currentHumidityDisplay").html("Humidity: " + response.list[0].main.humidity + "%")
-            $(".currentwindspeedDisplay").html("Wind speed: " + response.list[0].wind.speed + "mph")
-            $(".wind").text("Wind Speed: " + response.wind.speed);
-            $(".humidity").text("Humidity: " + response.main.humidity);
+
+            console.log("response.city.coord.lat")
+            console.log(response);
 
         });
-
-}
-
-$.ajax({
-    url: ("https://api.openweathermap.org/data/2.5/weather?zip=02066,us&appid="+APIKey),
-    method: "GET"
-})
-    // We store all of the retrieved data inside of an object called "response"
-    .then(function (response) {
-
-        console.log("response.city.coord.lat")
-        console.log(response);
-        firstAjaxCall();
-
-    });
+// }
 
 document.getElementById("search-btn").addEventListener("click", function (event) {
     event.preventDefault();
     console.log('button clicked');
+    firstAjaxCall();
 }
 );
