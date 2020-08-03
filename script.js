37d0c4f5795e089854bcdd6249261fef3b7a386b
Brian OLeary <oleary.brian1@gmail.com>
2020-08-02 21:41:03 -0400

    search bar working

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var APIKey = "3fa2a0131565ebb35436afe8cff1d40b";
+let zipCode = "02108";
 
 // Here we are building the URL we need to query the database
 // var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
@@ -15,6 +16,15 @@ var APIKey = "3fa2a0131565ebb35436afe8cff1d40b";
 
 const dayContainer = $('div.fiveDay');
 const uvIndexContainer = $('h6.uvIndex');
+const zipCodeSearch = document.getElementById('searchText');
+let townName = ""
+
+
+const searchBtn = document.getElementById("search-btn");
+
+zipCodeSearch.addEventListener("keyup", () => { //if there is text in the username box, save score button is enabled
+    searchBtn.disabled = !zipCodeSearch.value
+});
 
 
 
@@ -26,7 +36,6 @@ function secondAjaxCall(obj) {
         method: "GET"
     })
         .then(function (responsetwo) {
-            console.log(responsetwo)
 
             var weatherImg = ("http://openweathermap.org/img/wn/" + responsetwo.current.weather[0].icon + "@2x.png")
 
@@ -55,16 +64,15 @@ function secondAjaxCall(obj) {
             else {
                 highlightColor = '#ffffff'
             };
-            const indexdisplay = $('<span>').attr('style', ("background-color: " + highlightColor)).text(responsetwo.current.uvi);
 
-            uvIndexContainer.append(indexdisplay);
+            $(".uvIndex").html("UV Index: "+ "<span style= background-color:" + highlightColor + '>' + responsetwo.current.uvi + "</span>");
+
 
             //////////////////////5 day forcast below////////////////////////////////
 
             let i;
             for (i = 1; i < 6; i++) {
                 var dateString = moment.unix(responsetwo.daily[i].dt).format("MM/DD/YYYY");
-                console.log(dateString)
 
                 const dayDiv = $('<div>').addClass('card text-white bg-info mb-3 fiveDayItem card');
                 const dayCardBodyDiv = $('<div>').addClass('card-body');
@@ -82,8 +90,7 @@ function secondAjaxCall(obj) {
 
         });
 }
-let zipCode = "02066"
-// function firstAjaxCall() {
+function firstAjaxCall() {
     $.ajax({
         url: (`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${APIKey}`),
         method: "GET"
@@ -99,16 +106,26 @@ let zipCode = "02066"
                 longitude: longitude
             }
             secondAjaxCall(location);
-
-            console.log("response.city.coord.lat")
-            console.log(response);
+            townName = response.name;
+            saveRecentSearch();
 
         });
-// }
+}
+firstAjaxCall(); //find weather for the original zip code
 
-document.getElementById("search-btn").addEventListener("click", function (event) {
+searchBtn.addEventListener("click", function (event) {
     event.preventDefault();
-    console.log('button clicked');
+    dayContainer.empty();
+    zipCode = zipCodeSearch.value
     firstAjaxCall();
+    zipCodeSearch.value = ""
 }
 );
+
+function saveRecentSearch(){
+    const RecentSearch = {
+        town: townName,
+        zip: zipCode
+        }
+    console.log(RecentSearch)
+};
